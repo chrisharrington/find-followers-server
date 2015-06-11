0945535eac14781c40fdb696f73dd046b1bb28d0
Chris Harrington <chrisharrington99@gmail.com>
2015-06-10 19:39:22 -0600

    User credentials are now stripped before being sent back to the client.

--- test.js
+++ test.js
@@ -3,38 +3,30 @@
 require("./lib/globals");
 
 var Pipeline = include("pipeline"),
-	OAuth = require("oauth").OAuth,
-	secretConfig = include("secret");
-
-var token = "-cxgkgAAAAAAgAb5AAABTcTo17s",
-	secret = "2HPigiA6fZRYY4RZ3B38H5R3TIgojI8x",
-	verifier = "Yukvo2xTamVoz0iQKAwiCpVNIAMPUgpz";
+    User = include("data/models").User,
+	secretConfig = include("secret"),
+    _ = require("lodash");
 
 var accessToken = "14772674-fs5wiCen4dFIFmvtnS8xwwDZSpJWQP2U7RkbrLsFN",
 	accessSecret = "qKwfVxETf1KgyXiaAPHs1ARX3oXdi31fzUswD8j3fBGT1";
 
-var oauth = new OAuth(
-	"https://api.twitter.com/oauth/request_token", 
-	"https://api.twitter.com/oauth/access_token", 
-	secretConfig.twitter.consumerKey,
-	secretConfig.twitter.consumerSecret,
-	"1.0A",
-	null,
-	"HMAC-SHA1"
-);
-
 include("data").initialize().then(function() {
-	new Pipeline([
-	//	include("controllers/sign-in/access-token/get-access-token")
-		include("controllers/sign-in/access-token/verify-credentials"),
-		include("controllers/sign-in/access-token/save-user")
-	]).go(oauth, accessToken, accessSecret).then(function(a, b, c) {
-		console.log(a);
-		if (b)
-			console.log(b);
-		if (c)
-			console.log(c);
-	}).catch(function(err) {
-		console.error(err.stack);
-	});
+    User.findOne({ name: "Chris Harrington" }, function(err, user) {
+        if (err)
+            console.error(err);
+        else
+            include("tasks/twitter-favourites").go(user).then(function(result) {
+                console.log(_.map(result, function(tweet) {
+                    return {
+                        id: tweet.id,
+                        date: tweet.created_at,
+                        weight: tweet.weight
+                    };
+                }));
+            }).catch(function(err) {
+                console.error(err.stack);
+            }).finally(function() {
+                process.exit();
+            });
+    });
 });
