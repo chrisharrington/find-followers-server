bdc790f32a141b6475152c7541393b10bba627c6
Chris Harrington <chrisharrington99@gmail.com>
2015-06-11 12:05:56 -0600

    Switched from oauth library to node-twitter-api to solve weird access token issues.

--- test.js
+++ test.js
@@ -5,28 +5,41 @@ require("./lib/globals");
 var Pipeline = include("pipeline"),
     User = include("data/models").User,
 	secretConfig = include("secret"),
-    _ = require("lodash");
+    _ = require("lodash"),
+	Twitter = require("node-twitter-api");
 
-var accessToken = "14772674-fs5wiCen4dFIFmvtnS8xwwDZSpJWQP2U7RkbrLsFN",
-	accessSecret = "qKwfVxETf1KgyXiaAPHs1ARX3oXdi31fzUswD8j3fBGT1";
+//include("data").initialize().then(function() {
+//    var token = "7uiAhAAAAAAAgAb5AAABTeOu6vs",
+//		secret = "XjJlAt0Mkau758vrnVcGZrXm7bFZTp7l",
+//		verifier = "nynnLg6CrTQjDhKoWqqENpSZm4UlLAIi";
+//	
+//	new Pipeline([
+//		include("controllers/sign-in/access-token/get-access-token"),
+//		include("controllers/sign-in/access-token//verify-credentials"),
+//		include("controllers/sign-in/access-token//save-user"),
+//		include("controllers/sign-in/access-token//strip-credentials")
+//	]).go(oauth, token, secret, verifier).then(function(user) {
+//		console.log(user);
+//	}).catch(function(err) {
+//		console.error(err);
+//	}).finally(function() {
+//		process.exit();
+//	});
+//});
 
-include("data").initialize().then(function() {
-    User.findOne({ name: "Chris Harrington" }, function(err, user) {
-        if (err)
-            console.error(err);
-        else
-            include("tasks/twitter-favourites").go(user).then(function(result) {
-                console.log(_.map(result, function(tweet) {
-                    return {
-                        id: tweet.id,
-                        date: tweet.created_at,
-                        weight: tweet.weight
-                    };
-                }));
-            }).catch(function(err) {
-                console.error(err.stack);
-            }).finally(function() {
-                process.exit();
-            });
-    });
+var twitter = new Twitter({
+	consumerKey: secretConfig.twitter.consumerKey,
+	consumerSecret: secretConfig.twitter.consumerSecret,
+	callback: "http://findfollowers.me/#/oauth_callback"
+});
+
+var token = "mJk9xwAAAAAAgAb5AAABTePEyBU",
+	secret = "pDnWYAED6iRSj5RoXdzO7tzz9v2zzEQl",
+	verifier = "JnsRbRtuvOmTLuzRwW3OOrZC14MhIcnI";
+
+twitter.getAccessToken(token, secret, verifier, function(err, accessToken, accessTokenSecret) {
+	if (err)
+		console.error(err);
+	else
+		console.log(accessToken);
 });
