4c7f86e825994c44ffa8a60d2778e9f0c96de935
Chris Harrington <chrisharrington99@gmail.com>
2015-07-06 22:01:12 -0600

    Started work on recording historical data for statistical analysis.

--- test.js
+++ test.js
@@ -2,7 +2,9 @@
 
 require("./lib/globals")
 
-var User = include("data/models").User,
+var twitter = include("twitter-oauth"),
+
+    User = include("data/models").User,
 
     favourites = include("tasks/twitter-favourites"),
 
@@ -10,12 +12,33 @@ var User = include("data/models").User,
 
 include("data").initialize().then(function() {
     User.findOne({}, function(err, user) {
-        favourites.go(user).then(function(tweets) {
+        favourites.go(user).then(function(tweets ) {
             console.log(tweets);
         }).catch(function(err) {
-            console.log(err);
+            console.log(err.stack);
         }).finally(function() {
             process.exit();
         });
     });
 });
+
+function _get(user, screenName, ids, cursor) {
+    return new Promise(function(resolve, reject) {
+        ids = ids || [];
+        twitter.followers("ids", { screen_name: screenName, cursor: cursor }, user.token, user.secret, function(err, results) {
+            if (err)
+                reject(err);
+            else {
+                ids = ids.concat(results.ids);
+
+                cursor = results.next_cursor_str;
+                if (cursor !== "0")
+                    return _get(user, screenName, ids, cursor).then(function() {
+                        resolve(ids);
+                    });
+
+                resolve(ids);
+            }
+        });
+    });
+}
